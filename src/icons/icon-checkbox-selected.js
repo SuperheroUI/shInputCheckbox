826512acf8585cc37d1fa665bc283066bb3c4423
Cody Harrison <cody.harrison@its-macbook-pro.ad.insidesales.com>
2017-02-03 08:47:52 -0700

    changed over the svg but cant get the box to work

--- src/icons/icon-checkbox-selected.js
+++ src/icons/icon-checkbox-selected.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const IconCheckboxSelected = () => {
     return (
-        <svg className="icon-svg" style={{ height: 20, width: 20 }} preserveAspectRatio="xMidYMid meet" viewBox="25 25 50 50">
-            <g>
-                <path d="M68,29c1.7,0,3,1.3,3,3v36c0,1.7-1.3,3-3,3H32c-1.7,0-3-1.3-3-3V32c0-1.7,1.3-3,3-3H68 M68,26H32c-3.3,0-6,2.7-6,6v36 c0,3.3,2.7,6,6,6h36c3.3,0,6-2.7,6-6V32C74,28.7,71.3,26,68,26L68,26z" />
-                <path d="M36,51.7L48.7,63L66,39.4L60.8,36L47.7,53.9l-7.4-6.6L36,51.7L36,51.7z" />
-            </g>
+        <svg style="width:20px;height:20px" viewBox="25 25 50 50">
+            <path fill="#000000" d="M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" />
         </svg>
+
+        // <svg className="icon-svg" style={{ height: 20, width: 20 }} preserveAspectRatio="xMidYMid meet" viewBox="25 25 50 50">
+        //     <g>
+        //         <path d="M68,29c1.7,0,3,1.3,3,3v36c0,1.7-1.3,3-3,3H32c-1.7,0-3-1.3-3-3V32c0-1.7,1.3-3,3-3H68 M68,26H32c-3.3,0-6,2.7-6,6v36 c0,3.3,2.7,6,6,6h36c3.3,0,6-2.7,6-6V32C74,28.7,71.3,26,68,26L68,26z" />
+        //         <path d="M36,51.7L48.7,63L66,39.4L60.8,36L47.7,53.9l-7.4-6.6L36,51.7L36,51.7z" />
+        //     </g>
+        // </svg>
     )
 };
 
--- src/icons/icon-checkbox-unselected.js
+++ src/icons/icon-checkbox-unselected.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const IconCheckboxUnselected = () => {
     return (
-        <svg className="icon-svg" style={{ height: 20, width: 20 }} preserveAspectRatio="xMidYMid meet" viewBox="25 25 50 50">
-            <g>
-                <path d="M68,29c1.7,0,3,1.3,3,3v36c0,1.7-1.3,3-3,3H32c-1.7,0-3-1.3-3-3V32c0-1.7,1.3-3,3-3H68 M68,26H32c-3.3,0-6,2.7-6,6v36 c0,3.3,2.7,6,6,6h36c3.3,0,6-2.7,6-6V32C74,28.7,71.3,26,68,26L68,26z" />
-            </g>
+        <svg style="width:20px;height:20px" viewBox="25 25 50 50">
+            <path fill="#000000" d="M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z" />
         </svg>
+        // <svg className="icon-svg" style={{ height: 20, width: 20 }} preserveAspectRatio="xMidYMid meet" viewBox="25 25 50 50">
+        //     <g>
+        //         <path d="M68,29c1.7,0,3,1.3,3,3v36c0,1.7-1.3,3-3,3H32c-1.7,0-3-1.3-3-3V32c0-1.7,1.3-3,3-3H68 M68,26H32c-3.3,0-6,2.7-6,6v36 c0,3.3,2.7,6,6,6h36c3.3,0,6-2.7,6-6V32C74,28.7,71.3,26,68,26L68,26z" />
+        //     </g>
+        // </svg>
     )
 };
 
