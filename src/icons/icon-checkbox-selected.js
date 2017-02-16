92f5d9113a9840014e5a06e122150361e3bddf59
cody.harrison <cody.d.h.harrison@gmail.com>
2017-02-16 11:12:18 -0700

    changed the height and width to 20 for the designers

--- src/icons/icon-checkbox-selected.js
+++ src/icons/icon-checkbox-selected.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IconCheckboxSelected = () => {
     return (
-        <svg viewBox="0 0 24 24" style={{ height: 24, width: 24 }} >
+        <svg viewBox="0 0 24 24" style={{ height: 20, width: 20 }} >
             <path d="M21.3,0H2.7C1.2,0,0,1.2,0,2.7v18.7C0,22.8,1.2,24,2.7,24h18.7c1.5,0,2.7 -1.2,2.7 -2.7V2.7C24,1.2,22.8,0,21.3,0z M9.3,18.7L2.7,12l1.9 -1.9l4.8,4.8L19.5,4.8l1.9,1.9L9.3,18.7z"/>
         </svg>
     )
